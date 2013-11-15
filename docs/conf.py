4ba37dfc654cc6276333d6d904ad2b1006344b40
blink1073 <steven.silvester@ieee.org>
2013-11-15 04:27:33 -0600

    Changes made to work around build_sphinx error

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ release = version
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
-exclude_trees = ['_build']
+exclude_trees = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
