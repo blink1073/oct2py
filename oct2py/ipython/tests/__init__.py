dcecccf26059d5f86736a7411bb4dbfc0e7daca0
user.email <steven.silvester@ieee.org>
2013-12-07 11:43:13 -0600

    Add pragma statements

--- oct2py/ipython/tests/__init__.py
+++ oct2py/ipython/tests/__init__.py
@@ -1,11 +1,11 @@
 import unittest
 from os.path import dirname
 
-def main():
+def main():  # pragma: no cover
     tr = unittest.TextTestRunner(verbosity = 2)
     suite = unittest.defaultTestLoader.discover(dirname(__file__),
                                                 pattern='test*')
     tr.run(suite)
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: no cover
     main()
