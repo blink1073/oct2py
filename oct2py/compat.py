7c94101d3f81d69949506e25eb19201f8ae31098
user.email <steven.silvester@ieee.org>
2013-12-20 15:20:14 -0600

    Make clearer and add StringIO support

--- oct2py/compat.py
+++ oct2py/compat.py
@@ -5,9 +5,11 @@ PY2 = sys.version[0] == '2'
 PY3 = sys.version[0] == '3'
 
 
-if not PY2:  # pragma : no cover
-    unicode = str
-    long = int
-else:  # pragma : no cover
+if PY2:
     unicode = unicode
     long = long
+    from StringIO import StringIO
+else:  # pragma : no cover
+    unicode = str
+    long = int
+    from io import StringIO
