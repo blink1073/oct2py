fabb93204661c060f2416a4b9f111c58928d450e
blink1073 <steven.silvester@ieee.org>
2014-01-01 08:30:54 -0600

    Remove input from compat file

--- oct2py/compat.py
+++ oct2py/compat.py
@@ -9,11 +9,9 @@ if PY2:
     unicode = unicode
     long = long
     from StringIO import StringIO
-    input = raw_input
     import Queue as queue
 else:  # pragma : no cover
     unicode = str
     long = int
     from io import StringIO
-    input = input
     import queue
