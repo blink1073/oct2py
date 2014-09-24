4c9adcf00eb72b787b48d709a3d2942980ac0b1a
Steven Silvester <steven.silvester@ieee.org>
2014-09-24 12:10:10 -0500

    Add test for multiline output with two ans statements.

--- oct2py/tests/test_misc.py
+++ oct2py/tests/test_misc.py
@@ -236,3 +236,20 @@ class MiscTests(test.TestCase):
     def test_clear(self):
         """Make sure clearing variables does not mess anything up."""
         self.oc.clear()
+
+    def test_multiline_statement(self):
+        sobj = StringIO()
+        hdlr = logging.StreamHandler(sobj)
+        hdlr.setLevel(logging.DEBUG)
+        self.oc.logger.addHandler(hdlr)
+
+        self.oc.logger.setLevel(logging.DEBUG)
+
+        ans = self.oc.eval("""
+    a =1
+    a + 1
+    b = 3
+    b + 1""")
+        text = hdlr.stream.getvalue().strip()
+        assert ans == 4
+        assert text.endswith('\na =  1\nans =  2\nb =  3')
