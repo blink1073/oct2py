75bfee779bc5eb74ade4288532d2be973f4b100e
Steven Silvester <steven.silvester@ieee.org>
2016-12-24 09:10:21 -0600

    Bump version

--- oct2py/__init__.py
+++ oct2py/__init__.py
@@ -19,7 +19,7 @@ trust a code translator, this is your library.
 """
 
 __title__ = 'oct2py'
-__version__ = '3.6.5'
+__version__ = '3.7.0'
 __author__ = 'Steven Silvester'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014-2016 Steven Silvester'
