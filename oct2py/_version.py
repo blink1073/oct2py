fb8e170c95fdcaf891888f2f99b1d33d5b252144
Steven Silvester <steven.silvester@ieee.org>
2021-12-09 07:58:35 -0600

    update version

--- oct2py/_version.py
+++ oct2py/_version.py
@@ -11,7 +11,7 @@ VersionInfo = namedtuple('VersionInfo', [
     'serial'
 ])
 
-version_info = VersionInfo(5, 2, 0, "final", 0)
+version_info = VersionInfo(5, 3, 0, "final", 0)
 
 __version__ = '{}.{}.{}'.format(
     version_info.major,
