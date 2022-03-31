6f7a397f5a49703384f73cd8a84ebfef60b47ed4
Steven Silvester <steven.silvester@ieee.org>
2022-03-31 04:24:38 -0500

    use myst parser

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ import datetime
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
