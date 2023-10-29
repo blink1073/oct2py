0d466a176105ab052bc0eea8713c105d12122669
Steven Silvester <steven.silvester@ieee.org>
2023-10-29 15:42:34 -0500

    adopt ruff format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,6 +101,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "pydata_sphinx_theme"
+html_theme_options = {"navigation_with_keys": False}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
